6088aa456f1f4b2908c12a628a33fe5fc12c9fb3
Hamza Taous <hamzataous847@gmail.com>
2023-05-18 21:31:12 +0000

    Update views.py

--- RPA/views.py
+++ RPA/views.py
@@ -17,7 +17,7 @@ def UI(request):
             BFM = date.today() + relativedelta(months=-5)
             Company = request.POST['company']
             Chrome_options = webdriver.ChromeOptions()
-            Chrome_options.add_argument('--headless=new')
+            #Chrome_options.add_argument('--headless=new')
             chromedriver_path = '/usr/bin/chromedriver'
             driver = webdriver.Chrome(executable_path=chromedriver_path, options=Chrome_options)
             driver.maximize_window()
